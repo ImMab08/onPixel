1fd407b59956539ad53b3ddc3f94ac219f43da75
Franky Vargas <frankyvargas15@gmail.com>
2024-03-06 11:29:21 -0500

    0.1.2

--- src/components/shared/Header/Header.tsx
+++ src/components/shared/Header/Header.tsx
@@ -1,20 +1,34 @@
-'use client'
-import React, { useEffect, useState } from "react"
-import Image from "next/image"
-import Link from "next/link"
+"use client";
+import React from "react";
+import { FloatingNav } from "./floating-navbar";
+import { HiOutlineHome } from "react-icons/hi";
+import { FaCodeBranch } from "react-icons/fa";
+import { FaCode } from "react-icons/fa6";
 
 export const Header = () => {
-  const [menuOpen, setMenuOpen] = useState(false);
+  const navItems = [
+    {
+      name: "Home",
+      link: "/",
+      icon: <HiOutlineHome className="h-4 w-4 text-neutral-500 dark:text-white" />,
+    },
+    {
+      name: "About",
+      link: "/about",
+      icon: <FaCodeBranch className="h-4 w-4 text-neutral-500 dark:text-white" />,
+    },
+    {
+      name: "Contact",
+      link: "/contact",
+      icon: (
+        <FaCode className="h-4 w-4 text-neutral-500 dark:text-white" />
+      ),
+    },
 
-  const toggleMenu = () => {
-    setMenuOpen(!menuOpen);
-  };
-
-  const closeMenu = () => {
-    setMenuOpen(false);
-  };
-
+  ];
   return (
-    <></>
+    <div className="md:hidden relative  w-full">
+      <FloatingNav navItems={navItems} />
+    </div>
   );
 }
