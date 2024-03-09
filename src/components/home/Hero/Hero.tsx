5a6f7bb4cf104b3b7bd929b922705201487231bf
Franky Vargas <frankyvargas15@gmail.com>
2024-03-08 23:51:50 -0500

    0.1.4

--- src/components/home/Hero/Hero.tsx
+++ src/components/home/Hero/Hero.tsx
@@ -1,6 +1,9 @@
 "use client";
 import React from "react";
-import { useState, useEffect } from 'react';
+import Image from "next/image";
+import { useState, useEffect } from "react";
+import { clearInterval } from "timers";
+import { animate } from "framer-motion";
 
 export const Hero = () => {
   // const [wordIndex, setWordIndex] = useState(0);
@@ -15,14 +18,33 @@ export const Hero = () => {
   // }, [words.length]);
 
   return (
-    <section className="container mx-auto  items-center h-screen flex">
-      <div className="w-1/2">
-      </div>
-      <div className="w-1/2">
+    <section className=" mx-10 md:mx-20 items-center h-screen flex flex-col justify-center text-hero ">
+      <div className="w-full md:flex">
+        <div className="w-1/2 mx-auto flex items-center justify-center flex-col mt-20">
+          <h1 className="text-white text-center text-5xl md:text-7xl font-semibold">
+            Transformamos ideas en soluciones digitales excepcionales.
+          </h1>
+          <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 mt-10 md:mt-20">
+            <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#00ff26_0%,#393BB2_50%,#00ff26_100%)]" />
+            <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+              Contactanos ahora
+            </span>
+          </button>
+        </div>
+
+        <div className="w-1/2 hidden md:block relative top-10 left-10">
+          <Image
+            src="/img/fondo-hero.png"
+            className=""
+            width={600}
+            height={500}
+            alt=""
+          />
+        </div>
       </div>
     </section>
   );
-}
+};
 
 // const TransitionWord = ({ word }) => {
 //   return (
