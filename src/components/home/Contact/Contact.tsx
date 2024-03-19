ea42fcc686a6cfb2d472b6b2162a112dd9fa0886
Franky Vargas <frankyvargas15@gmail.com>
2024-03-19 01:48:02 -0500

    0.0.1

--- src/components/home/Contact/Contact.tsx
+++ src/components/home/Contact/Contact.tsx
@@ -1,39 +1,10 @@
 "use client";
-import { TypewriterEffectSmooth } from "./typewriter-effect";
 
 export const Contact = () => {
-  const words = [
-    {
-      text: "Impulsa",
-      className: "text-white text-6xl",
-    },
-    {
-      text: "tus",
-      className: "text-white text-6xl",
-    },
-    {
-      text: "ideas",
-      className: "text-white text-6xl",
-    },
-    {
-      text: "Ahora.",
-      className: "section-title text-6xl",
-    },
-  ];
   return (
-    <div className="flex flex-col items-center justify-center h-screen  ">
-      <p className="text-neutral-400 text-lg   ">
-        No esperes más para contactarnos
-      </p>
-      <TypewriterEffectSmooth words={words} />
-      <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4">
-        <a target="_blank" href="https://www.workana.com/freelancer/8f0b32ac61fc53c2da34af5335950a68" className="w-40 h-10 rounded-xl bg-white text-black border border-black flex items-center justify-center text-bold  text-sm">
-          Workana
-        </a>
-        <a href="" className="w-40 h-10 rounded-xl bg-white text-black border border-black flex items-center justify-center text-bold text-sm">
-          Correo
-        </a>
-      </div>
-    </div>
-  );
+    <section className="h-screen">
+
+    </section>
+  )
 }
+
--- src/components/home/Hero/Hero.tsx
+++ src/components/home/Hero/Hero.tsx
@@ -1,21 +1,8 @@
 "use client";
 import React from "react";
 import Image from "next/image";
-import { useState, useEffect } from "react";
-import { clearInterval } from "timers";
-import { animate } from "framer-motion";
 
 export const Hero = () => {
-  // const [wordIndex, setWordIndex] = useState(0);
-  // const words = ['Software', 'Webs', 'fantastic', 'incredible']; // Lista de palabras
-
-  // useEffect(() => {
-  //   const interval = setInterval(() => {
-  //     setWordIndex((prevIndex) => (prevIndex + 1) % words.length); // Cambia la palabra cada cierto tiempo
-  //   }, 2000); // Cambia cada 2 segundos (ajusta según sea necesario)
-
-  //   return () => clearInterval(interval); // Limpia el intervalo cuando el componente se desmonta
-  // }, [words.length]);
 
   return (
     <section className="w-screen h-screen flex items-center py-auto px-5 md:mx-20  text-hero ">
@@ -23,12 +10,12 @@ export const Hero = () => {
         <h1 className="text-white text-center md:text-left text-4xl md:text-7xl font-semibold">
           Transformamos ideas en soluciones digitales excepcionales.
         </h1>
-        <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 mt-10 md:mt-20">
+        <a href="https://www.workana.com/freelancer/8f0b32ac61fc53c2da34af5335950a68" className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 mt-10 md:mt-20">
           <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#00ff26_0%,#393BB2_50%,#00ff26_100%)]" />
           <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-5 py-3 text-lg font-medium text-white backdrop-blur-3xl">
             Contactanos ahora
           </span>
-        </button>
+        </a>
       </div>
       <div className="w-1/2 hidden md:block relative top-10 left-10">
         <Image
@@ -42,11 +29,3 @@ export const Hero = () => {
     </section>
   );
 };
-
-// const TransitionWord = ({ word }) => {
-//   return (
-//     <span className="inline-block" key={word}>
-//       {word}
-//     </span>
-//   );
-// }
